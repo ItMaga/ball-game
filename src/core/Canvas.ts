30bbca5a9124fe306ea05f263e9be18ab341be5b
ItMaga <m-chemurziev@ya.ru>
2021-11-14 15:13:15 +0300

    Updates

--- src/core/Canvas.ts
+++
@@ -1,6 +0,0 @@
-export default class Canvas {
-  private static instance = <HTMLCanvasElement>document.getElementById('canvas');
-  public static context = this.instance.getContext('2d');
-  public static width = this.instance.width;
-  public static height = this.instance.height;
-}
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import Game from './core/Game';
-
-import './styles/index.css';
-
-new Game();
-console.log('Hello World!');
